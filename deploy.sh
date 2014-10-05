3396b8fe08f13501398e45389cf8715963b0afd5
Nawaid Shamim <nawaid17@gmail.com>
2014-10-05 17:22:49 +0100

    Updating deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,19 @@
 
 if [ ! -f "api.json" ];
 then
-   echo "Please create api.json before deploying to App Engine"
-   exit 1
+  echo "Please create api.json before deploying to App Engine"
+  exit 1
 fi
 
 # checkout submodules
-git submodule init
-git submodule update
+if [ ! -d "static/web-starter-kit" ] || [ ! -d "static/devfest-2014" ];
+then
+  git submodule init
+  git submodule update
+fi
+
+# build devfest site
+jekyll build --source static/devfest-2014 --destination static/devfest
 
 # deploy to App Engine
-appcfg.py update .
+appcfg.py -v update .
