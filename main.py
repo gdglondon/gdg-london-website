86941445c19aacaf399a56cdea1386c9e5cc7a46
Radek Simko <radek.simko@gmail.com>
2015-04-04 12:24:19 +0100

    pylint warnings/errors fixed

--- main.py
+++ main.py
@@ -3,94 +3,91 @@ import os
 import webapp2
 import urllib2
 import json
-import pickle
-import cgi
+import jinja2
+import datetime
 
-from webapp2_extras import *
-from google.appengine.api import mail
 from google.appengine.ext import db
 from google.appengine.api import memcache
 from webapp2_extras import routes
-import jinja2
-import datetime
-jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+JINJA = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 with open(os.path.join(os.path.dirname(__file__), "api.json"), "r") as f:
-  APIJSON = json.load(f)
+    APIJSON = json.load(f)
 
-if 'Development' in os.environ['SERVER_SOFTWARE']:
-	siteURL = 'http://localhost:8080/'
-	siteDomain = 'localhost'
-else:
-	siteURL = 'http://www.gdg-london.com/'
-	siteDomain = 'www.gdg-london.com'
 
 class GooglePlusPosts(db.Model):
-  PostTimestamp = db.DateTimeProperty(required=True)
-  PostJson = db.TextProperty(required=True)
-  LastUpdated = db.DateTimeProperty(auto_now_add=True)
+    PostTimestamp = db.DateTimeProperty(required=True)
+    PostJson = db.TextProperty(required=True)
+    LastUpdated = db.DateTimeProperty(auto_now_add=True)
 
-class Helper:
-  def __init__(self):
-    self.profileId = "+Gdg-london"
-    self.apiKey = APIJSON["key"]
 
-  def storeGooglePlusPosts(self):
-    apiendpoint = "https://www.googleapis.com/plus/v1/people/" \
-                  + self.profileId + \
-                  "/activities/public?key=" + self.apiKey
+class Helper(object):
+    def __init__(self):
+        self.profile_id = "+Gdg-london"
+        self.api_key = APIJSON["key"]
 
-    response = urllib2.urlopen(apiendpoint, timeout=20).read()
-    jsonResponse = json.loads(response)
-    for item in jsonResponse["items"]:
-        postTimestamp = datetime.datetime.strptime(item["updated"][:-5], "%Y-%m-%dT%H:%M:%S")
-        GooglePlusPosts(key_name=item["id"], PostJson=unicode(json.dumps(item), 'utf-8'),
-                        PostTimestamp=postTimestamp).save()
+    def store_google_plus_posts(self):
+        apiendpoint = "https://www.googleapis.com/plus/v1/people/" \
+                      + self.profile_id + \
+                      "/activities/public?key=" + self.api_key
 
-  def getGooglePlusPosts(self):
-    #connect datastore and call function to get html for each post, do exception handling here
-    response = memcache.get("GooglePlusPosts")
-    if not response:
-        q = db.GqlQuery("SELECT * FROM GooglePlusPosts ORDER BY PostTimestamp DESC LIMIT 20")
-        posts = []
-        for item in q:
-            posts.append(json.loads(item.PostJson))
-        response = json.dumps(posts)
-        memcache.add("GooglePlusPosts", response, 60)
-    postsInHTML = []
-    posts = json.loads(response)
-    for i in range(0,len(posts)):
-      post = posts[i]
-      #try:
-      postinHTML = self.generateHTMLGooglePlusPost(post, i)
-      postsInHTML.append(postinHTML)
-      #except:
-      postsInHTML.append("<!-- Failed to parse " + post["id"] + "-->")
-    return postsInHTML
+        response = urllib2.urlopen(apiendpoint, timeout=20).read()
+        json_response = json.loads(response)
 
-  def generateHTMLGooglePlusPost(self, post, i ):
+        for item in json_response["items"]:
+            post_timestamp = datetime.datetime.strptime(item["updated"][:-5], "%Y-%m-%dT%H:%M:%S")
+            GooglePlusPosts(key_name=item["id"], PostJson=unicode(json.dumps(item), 'utf-8'),
+                            PostTimestamp=post_timestamp).save()
+
+    def get_google_plus_posts(self):
+        #connect datastore and call function to get html for each post, do exception handling here
+        response = memcache.get("GooglePlusPosts")
+        if not response:
+            query = db.GqlQuery("SELECT * FROM GooglePlusPosts ORDER BY PostTimestamp DESC LIMIT 20")
+            posts = []
+            for item in query:
+                posts.append(json.loads(item.PostJson))
+            response = json.dumps(posts)
+            memcache.add("GooglePlusPosts", response, 60)
+        posts_in_html = []
+        posts = json.loads(response)
+
+        for i in range(0, len(posts)):
+            post = posts[i]
+            #try:
+            post_in_html = generate_html_google_plus_post(post, i)
+            posts_in_html.append(post_in_html)
+            #except:
+            posts_in_html.append("<!-- Failed to parse " + post["id"] + "-->")
+
+        return posts_in_html
+
+def generate_html_google_plus_post(post, i):
     ''' returns HTML for Google+ post and exception return None '''
-    template = jinja_environment.get_template('templates/googlepluspost.html')
+    template = JINJA.get_template('templates/googlepluspost.html')
     return template.render({"post": post, "i": i})
 
-
 class CronHandler(webapp2.RequestHandler):
-  def get(self):
-    Helper().storeGooglePlusPosts()
+    def get(self):
+        Helper().store_google_plus_posts()
+        self.response.write('')
 
 class GDGHandler(webapp2.RequestHandler):
-  def get(self):
-    self.response.headers['Content-Type'] = 'text/html'
-    # Generate Google+ post
-    posts = Helper().getGooglePlusPosts()
-    template = jinja_environment.get_template('templates/index.html')
-    self.response.write(template.render({"posts": posts}))
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        # Generate Google+ post
+        posts = Helper().get_google_plus_posts()
+        template = JINJA.get_template('templates/index.html')
+        self.response.write(template.render({"posts": posts}))
 
-URL_Mapping = [
-  webapp2.Route('/', handler=GDGHandler, name='GDG London'), 
-  routes.RedirectRoute('/devfest', redirect_to='http://devfest.gdg-london.com',
-    name='Devfest', strict_slash=True),
-  webapp2.Route('/cron/gplus', handler=CronHandler)
+URL_MAPPING = [
+    webapp2.Route('/', handler=GDGHandler, name='GDG London'),
+    routes.RedirectRoute('/devfest', redirect_to='http://devfest.gdg-london.com',
+                         name='Devfest', strict_slash=True),
+    webapp2.Route('/cron/gplus', handler=CronHandler)
 ]
 
-app = webapp2.WSGIApplication(URL_Mapping, debug=False)
+APP = webapp2.WSGIApplication(URL_MAPPING, debug=False)
